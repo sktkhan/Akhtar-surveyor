f5b42444db4a3a5d1b238b191fb944db681a49d6
sktkhan <akhtaralipk441@gmail.com>
2026-04-26 17:44:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function calculateRL() {
+    let bm = parseFloat(document.getElementById("bm").value);
+    let bs = parseFloat(document.getElementById("bs").value);
+    let fs = parseFloat(document.getElementById("fs").value);
 
+    let hi = bm + bs;
+    let rl = hi - fs;
+
+    document.getElementById("result").innerText =
+        "HI: " + hi + " | RL: " + rl;
+}
